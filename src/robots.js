bcb848d352fc3d3f81fd9efb051c8b80600d612e
Charles bellorini <charlesbellorini@imac-de-charles.lan>
2020-03-29 16:59:29 +0200

    First commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Jean Marc',
+        username: 'Geant',
+        email: 'Sincere@gugus.biz'
+    },
+    {
+        id: 3,
+        name: 'Marcel',
+        username: 'Durant',
+        email: 'Sincere@zaerty.biz'
+    },
+    {
+        id: 4,
+        name: 'Beavor JR',
+        username: 'Williams',
+        email: 'Sincere@melissa.biz'
+    },
+]
